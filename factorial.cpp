a2486a46216981c96c4bbf4d584f3e7d230a1d13
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-03-09 22:20:25 +0330

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -17,3 +17,21 @@ int main()
 
     cout << "factorial : " << factorial(n);
 }
+
+/*int main()
+{
+  int64_t factorial = 1;
+  int i = 1, number;
+  cout << "enter number : ";
+  cin >> number;
+  cout << "number\tfactorial" << endl;
+  cout << "------------------------" << endl;
+  do
+  {
+    factorial *= i;
+    cout << i << "\t" << factorial << endl;
+    i++;
+  } while (i <= number);
+
+    return 0;
+}*/
