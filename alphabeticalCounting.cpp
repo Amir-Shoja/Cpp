70480ad79082fcb339764b92401828d56fe88fd5
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-03-09 18:29:01 +0330

    Add files via upload

---
+++ alphabeticalCounting.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0, k = 0;
+    int l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, s = 0, t = 0, u = 0, v = 0;
+    int w = 0, x = 0, y = 0, z = 0, Q = 0, W = 0, E = 0, R = 0, T = 0, Y = 0, U = 0;
+    int I = 0, O = 0, P = 0, A = 0, S = 0, D = 0, F = 0, G = 0, H = 0, J = 0, K = 0;
+    int L = 0, M = 0, N = 0, B = 0, V = 0, C = 0, X = 0, Z = 0;
+    char ch;
+
+    while ((ch = cin.get()) != '.')
+    {
+        switch (ch)
+        {
+        case 'a':
+            ++a;
+            break;
+        case 'b':
+            ++b;
+            break;
+        case 'c':
+            ++c;
+            break;
+        case 'd':
+            ++d;
+            break;
+        case 'e':
+            ++e;
+            break;
+        case 'f':
+            ++f;
+            break;
+        case 'g':
+            ++g;
+            break;
+        case 'h':
+            ++h;
+            break;
+        case 'i':
+            ++i;
+            break;
+        case 'j':
+            ++j;
+            break;
+        case 'k':
+            ++k;
+            break;
+        case 'l':
+            ++l;
+            break;
+        case 'm':
+            ++m;
+            break;
+        case 'n':
+            ++n;
+            break;
+        case 'o':
+            ++o;
+            break;
+        case 'p':
+            ++p;
+            break;
+        case 'q':
+            ++q;
+            break;
+        case 'r':
+            ++r;
+            break;
+        case 's':
+            ++s;
+            break;
+        case 't':
+            ++t;
+            break;
+        case 'u':
+            ++u;
+            break;
+        case 'v':
+            ++v;
+            break;
+        case 'w':
+            ++w;
+            break;
+        case 'x':
+            ++x;
+            break;
+        case 'y':
+            ++y;
+            break;
+        case 'z':
+            ++z;
+            break;
+        case 'A':
+            ++A;
+            break;
+        case 'B':
+            ++B;
+            break;
+        case 'C':
+            ++C;
+            break;
+        case 'D':
+            ++D;
+            break;
+        case 'E':
+            ++E;
+            break;
+        case 'F':
+            ++F;
+            break;
+        case 'G':
+            ++G;
+            break;
+        case 'H':
+            ++H;
+            break;
+        case 'I':
+            ++I;
+            break;
+        case 'J':
+            ++J;
+            break;
+        case 'K':
+            ++K;
+            break;
+        case 'L':
+            ++L;
+            break;
+        case 'M':
+            ++M;
+            break;
+        case 'N':
+            ++N;
+            break;
+        case 'O':
+            ++O;
+            break;
+        case 'P':
+            ++P;
+            break;
+        case 'Q':
+            ++Q;
+            break;
+        case 'R':
+            ++R;
+            break;
+        case 'S':
+            ++S;
+            break;
+        case 'T':
+            ++T;
+            break;
+        case 'U':
+            ++U;
+            break;
+        case 'V':
+            ++V;
+            break;
+        case 'W':
+            ++W;
+            break;
+        case 'X':
+            ++X;
+            break;
+        case 'Y':
+            ++Y;
+            break;
+        case 'Z':
+            ++Z;
+            break;
+        default:
+            break;
+        }
+    }
+    cout << "a : " << a << endl
+         << "b : " << b << endl
+         << "c : " << c << endl
+         << "d : " << d << endl
+         << "e : " << e << endl
+         << "f : " << f << endl
+         << "g : " << g << endl
+         << "h : " << h << endl
+         << "i : " << i << endl
+         << "j : " << j << endl
+         << "k : " << k << endl
+         << "l : " << l << endl
+         << "m : " << m << endl
+         << "n : " << n << endl
+         << "o : " << o << endl
+         << "p : " << p << endl
+         << "q : " << q << endl
+         << "r : " << r << endl
+         << "s : " << s << endl
+         << "t : " << t << endl
+         << "u : " << u << endl
+         << "v : " << v << endl
+         << "w : " << w << endl
+         << "x : " << x << endl
+         << "y : " << y << endl
+         << "z : " << z << endl
+         << "A : " << A << endl
+         << "B : " << B << endl
+         << "C : " << C << endl
+         << "D : " << D << endl
+         << "F : " << F << endl
+         << "G : " << G << endl
+         << "H : " << H << endl
+         << "I : " << I << endl
+         << "J : " << J << endl
+         << "K : " << K << endl
+         << "L : " << L << endl
+         << "M : " << M << endl
+         << "N : " << N << endl
+         << "O : " << O << endl
+         << "P : " << P << endl
+         << "Q : " << Q << endl
+         << "R : " << R << endl
+         << "S : " << S << endl
+         << "T : " << T << endl
+         << "U : " << U << endl
+         << "V : " << V << endl
+         << "W : " << W << endl
+         << "X : " << X << endl
+         << "Y : " << Y << endl
+         << "Z : " << Z << endl;
+    return 0;
+}
