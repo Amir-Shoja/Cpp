f9242f3a7a632294a7fd1a4dd06e6ca71d8ee6c7
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-03-11 01:54:41 +0330

    Add files via upload

---
+++ GCD-HCF.cpp
@@ -0,0 +1,40 @@
+// Euclidean method(sequential division)
+
+#include <iostream>
+using namespace std;
+
+int gcd(int, int);
+
+int main()
+{
+    int num1, num2;
+    cout << "Entern tow number for find GCD :" << endl
+         << "Num1 : ";
+    cin >> num1;
+    cout << "Num2 : ";
+    cin >> num2;
+
+    cout << "we use Euclidean method for find gcd :" << endl
+         << "gcd is : ";
+    cout << gcd(num1, num2);
+
+    return 0;
+}
+
+int gcd(int x, int y)
+{
+    int r = x % y;
+
+    while (r != 0)
+    {
+        x = y;
+        y = r;
+        r = x % y;
+    }
+    return y;
+}
+
+/*int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}*/
