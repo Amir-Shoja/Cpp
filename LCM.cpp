e75a9f610800c2bb793654018132aa804a8a8ee4
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-04-06 22:40:47 +0430

    Update LCM.cpp

--- LCM.cpp
+++ LCM.cpp
@@ -16,3 +16,4 @@ int main()
     cin >> num1 >> num2;
     cout << "LCM = " << ((num1 * num2) / gcd(num1, num2));
 }
+//https://virgool.io/@The_lotonegro/%D8%A7%D9%84%DA%AF%D9%88%D8%B1%DB%8C%D8%AA%D9%85-%D8%A7%D9%82%D9%84%DB%8C%D8%AF%D8%B3-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%A8-%D9%85-%D9%85-%D9%88-%DA%A9-%D9%85-%D9%85-od9cdkpehuhr
