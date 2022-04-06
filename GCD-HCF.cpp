0cd72fb583fb07d77f3b218f7d7c6bad49f59a15
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-04-06 22:41:03 +0430

    Update GCD-HCF.cpp

--- GCD-HCF.cpp
+++ GCD-HCF.cpp
@@ -47,3 +47,4 @@ int gcd(int64_t x, int64_t y)
 {
     return b == 0 ? a : gcd(b, a % b);
 }*/
+//https://virgool.io/@The_lotonegro/%D8%A7%D9%84%DA%AF%D9%88%D8%B1%DB%8C%D8%AA%D9%85-%D8%A7%D9%82%D9%84%DB%8C%D8%AF%D8%B3-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%A8-%D9%85-%D9%85-%D9%88-%DA%A9-%D9%85-%D9%85-od9cdkpehuhr
